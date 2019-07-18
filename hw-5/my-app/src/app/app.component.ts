1bc3cdcc377b589dd690cedbea91680c5f9b4a07
Iryna Zabashta <irina.zabashta@requestum.com>
2019-07-18 18:46:34 +0300

    homework 5

--- hw-5/my-app/src/app/app.component.ts
+++ hw-5/my-app/src/app/app.component.ts
@@ -1,7 +1,4 @@
 import { Component } from '@angular/core';
-import { Observable, fromEvent } from 'rxjs';
-import { scan } from 'rxjs/operators';
-import {count} from "rxjs/internal/operators";
 
 @Component({
   selector: 'app-root',
