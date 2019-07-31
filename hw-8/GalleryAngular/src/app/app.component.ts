c73f97f8c0dd833bf3fb09cbcbd0c24e315dcd7d
irinazabashta <irina.zabashta@requestum.com>
2019-07-31 15:42:14 +0300

    homework 8

--- hw-8/GalleryAngular/src/app/app.component.ts
+++ hw-8/GalleryAngular/src/app/app.component.ts
@@ -9,29 +9,6 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'GalleryAngular';
 
-  public imagePath;
-  imgURL: any;
-  public message: string;
-
-  preview(files) {
-    // if (files.length === 0)
-    //   return;
-    //
-    // var mimeType = files[0].type;
-    // if (mimeType.match(/image\/*/) == null) {
-    //   this.message = "Only images are supported.";
-    //   return;
-    // }
-
-    const reader = new FileReader();
-    this.imagePath = files;
-    reader.readAsDataURL(files[0]);
-    reader.onload = (_event) => {
-      this.imgURL = reader.result;
-    };
-    console.log(reader);
-  }
-
   public gallery: GalleryInterface[] = [
     {
       title: "accusamus beatae ad facilis cum similique qui sunt",
@@ -64,9 +41,39 @@ export class AppComponent {
   public advancedClick($event): void {
     this.selectImageUrl = $event;
   }
+
+  imageInput: any;
+  public image: GalleryInterface;
+  public imageURL: any;
+  public message: string;
+
+  public uploadImage(files): void {
+
+    this.imageInput = files[0];
+    // if (files.length === 0)
+    //   return;
+    //
+    // var mimeType = files[0].type;
+    // if (mimeType.match(/image\/*/) == null) {
+    //   this.message = "Only images are supported.";
+    //   return;
+    // }
+
+    const reader = new FileReader();
+    reader.readAsDataURL(this.imageInput);
+    reader.onload = (e) => {
+      this.imageURL = reader.result;
+      this.image = {
+        title: this.imageInput.name,
+        url: this.imageURL
+      }
+    };
+
+    this.gallery.push(this.image);
+  }
 }
 
 interface GalleryInterface{
   title: string,
-  url: string
+  url: string | any
 }
